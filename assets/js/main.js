e667d5d0553d1894902da7ebafee55ed992fd1ad
rathanont <rathanont.s@gmail.com>
2019-08-01 20:22:12 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ $(function () {
     $('.banner-slide').slick({
         autoplay: true,
         dots: false,
-        prevArrow: '<a href="#" class="slick-arrow-prev"><img src="../assets/images/arrow-prev.png" /></a>',
-        nextArrow: '<a href="#" class="slick-arrow-next"><img src="../assets/images/arrow-next.png" /></a>',
+        prevArrow: '<a href="#" class="slick-arrow-prev"><img src="./assets/images/arrow-prev.png" /></a>',
+        nextArrow: '<a href="#" class="slick-arrow-next"><img src="./assets/images/arrow-next.png" /></a>',
     });
 });
